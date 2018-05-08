3af0d8650d4984d74a53b0acdaf63deabe2f06b4
Lukasz Majcher <lmajcher@andea.com>
2018-05-08 13:01:30 +0200

    Minor fixes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,13 @@
+import { WeaponService } from './../services/weapon.service';
+import { Weapon } from './../models/weapon';
+import { Ship } from './../models/ship';
 import { Component, OnInit, OnChanges } from '@angular/core';
 
 import { Character } from './../models/character';
 
 import { CharacterService } from '../services/character.service';
 import { ImageService } from './../services/image.service';
+import { ShipService } from '../services/ship.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,14 +17,31 @@ import { ImageService } from './../services/image.service';
 export class DashboardComponent implements OnInit {
 
   characters: Character[] = [];
-  visibleImages: any[] = [];
-  constructor(private characterService: CharacterService, private imageService: ImageService) {
-    this.visibleImages = this.imageService.getImages();
-   }
+  ships: Ship[] = [];
+  weapons: Weapon[] = [];
+
+  visibleImagesCharacters: any[] = [];
+  visibleImagesWeapons: any[] = [];
+  visibleImagesShips: any[] = [];
+
+  constructor(
+    private characterService: CharacterService,
+    private imageService: ImageService,
+    private shipService: ShipService,
+    private weaponService: WeaponService
+  ) {
+    this.visibleImagesCharacters = this.imageService.getImages();
+    this.visibleImagesShips = this.visibleImagesCharacters.slice(10, 12);
+    this.visibleImagesWeapons = this.visibleImagesCharacters.slice(12, 16);
+  }
 
   ngOnInit() {
     this.characterService.getCharacters()
-    .then(characters => this.characters = characters.slice(0, 4));
+      .then(characters => this.characters = characters.slice(0, 4));
+    this.shipService.getShips()
+      .then(ships => this.ships = ships);
+    this.weaponService.getWeapons()
+      .then(weapons => this.weapons = weapons.slice(0, 4));
   }
 
 
